d56cdb1c390c0acb3d60fddade44bc439d4912df
yayo.262002 <carlosmarchan2602@gmail.com>
2022-12-19 16:44:54 +0100

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import "./Home.scss";
 import React from 'react'
 import foto1 from '../../src/assets/200.jpeg'
-import mv1 from '../../src/assets/mv1.webp'
-import mv2 from '../../src/assets/mv2.webp'
-import mv3 from '../../src/assets/mv3.webp'
-import mv4 from '../../src/assets/mv4.webp'
+import proteh from '../../src/assets/proteh.webp'
+import myproh from '../../src/assets/myproh.webp'
+import veganosh from '../../src/assets/veganosh.webp'
+import vitah from '../../src/assets/vitah.webp'
+import creatinah from '../../src/assets/creatinah.webp'
+import barrash from '../../src/assets/barrash.webp'
 import rop1 from '../../src/assets/rop1.webp'
 import rop2 from '../../src/assets/rop2.webp'
 import rop3 from '../../src/assets/rop3.webp'
 import rop4 from '../../src/assets/rop4.webp'
 import invita from '../../src/assets/invita.webp'
 import telegram from '../../src/assets/telegram.webp'
-import start from '../../src/assets/starts.png'
-import start0 from '../../src/assets/start.png'
 import face from '../../src/assets/face.png'
 import tui from '../../src/assets/tui.png'
 import tik from '../../src/assets/tik.png'
@@ -31,99 +31,77 @@ const Home = () => {
     <img className='foto1' src={foto1}/>
     </div>
 </Link>
-<ul className='nav-home-ul'>
-            
-            <li>
-            <Link to="/proteinas">Proteinas</Link>
-            </li>
-            <li>
-            <Link to="/creatinas">Creatinas</Link>
-            </li>
-            <li>
-            <Link to="/snacks">Barritas y Snacks</Link>
-            </li>
-            <li>
-            <Link to="/vitaminas">Vitaminas</Link>
-            </li>
-            <li>
-            <Link to="/vegan">Gama Vegana</Link>
-            </li>
-            <li>
-            <Link to="/ropa">Ropa</Link>
-            </li>
-  </ul>
-  <div className="div-masvendidos">
-         <div className="div-masvendidos-h1">
-                <h1>Los mas Vendido</h1>
-        </div> 
 
-            <div className="div-masvendidos-products">
 
-                    <div className="card">
-                        <img src={mv1}/>
-                        <span className="comprarapida">Compra Rapida</span>
-                        <p>Impact Whey Protein</p>
-                        <div className="stars">
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        </div>
-                        <p className="oferta-mv"> ⚡OFERTA⚡ 30% DE DESCUENTO</p>
-                        <p className="oferta-mv-abajo">En los más vendidos | Código: MYP30</p>
-                        <p className="oferta-mv-precio">desde 11,99 €</p>
-                    </div>
+<div className="div-masvendidos-h1">
+                    <h1>Compra por Categorías</h1>
+            </div> 
 
+
+
+<div className="cards-cate">
+
+        <Link to="/proteinas">
+        <div className="card">
+        
+                <img src={proteh}/>
+                <h2>Proteina</h2>
+                <p>Descubre nuestros batidos de proteínas de alta calidad, incluida la proteína en polvo número 1 del Reino Unido, Impact Whey Protein</p>
                 
-                    <div className="card">
-                        <img src={mv2}/>
-                        <span className="comprarapida">Compra Rapida</span>
-                        <p>Creatina Monohidrato en Polvo</p>
-                        <div className="stars">
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start0}/>
-                        </div>
-                        <p className="oferta-mv-precio">desde 18,99 €</p>
-                    </div>
-
-                    <div className="card">
-                        <img src={mv3}/>
-                        <span className="comprarapida">Compra Rapida</span>
-                        <p>Clear Whey Isolate</p>
-                        <div className="stars">
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        </div>
-                        <p className="oferta-mv-precio">desde 29,99 €</p>
-                    </div>
-
-                    <div className="card">
-                        <img src={mv4}/>
-                        <span className="comprarapida">Compra Rapida</span>
-                        <p>Impact Whey Isolate</p>
-                        <div className="stars">
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start}/>
-                        <img src={start0}/>
-                        </div>
-                        <p className="oferta-mv"> ⚡OFERTA⚡ 30% DE DESCUENTO</p>
-                        <p className="oferta-mv-abajo">En los más vendidos | Código: MYP30</p>
-                        <p className="oferta-mv-precio">desde 30,99 €</p>
-                    </div>
         </div>
-                <div>
-                        <ControlledCarousel/>
-                </div>
-        </div>     
+        </Link>
+
+        <Link to="/vegan">
+        <div className="card">
+                <img src={veganosh}/>
+                <h2>Productos Veganos</h2>
+                <p>Para todos aquellos que siguen una dieta Vegana y quieren conseguir sus objetivos. Descubre nuestra gama de productos</p>
+        </div>
+        </Link> 
+
+        <Link to="/mypro">
+        <div className="card">
+                <img src={myproh}/>
+                <h2>Myprotein Pro</h2>
+                <p>
+        Descubre la gama premium, desarrollada a partir de ingredientes superiores que dan a luz a nuestros suplementos de la más alta calidad.</p>
+        </div>
+        </Link>
+
+        <Link to="/vitaminas">
+        <div className="card">
+                <img src={vitah}/>
+                <h2>Vitaminas y minerales</h2>
+                <p>
+        Nuestras vitaminas y minerales están aquí para que te veas y se sientas lo mejor posible, desde adentro hacia afuera</p>
+        </div>
+        </Link>
+
+        <Link to="/mypro">
+        <div className="card">
+                <img src={barrash}/>
+                <h2>Barritas de proteínas & Snacks</h2>
+                <p>
+        Para un impulso nutritivo entre horas o algo que te ayude a recuperarte para que estés listo para tu próxima sesión</p>
+        </div>
+        </Link>
+
+        <Link to="/creatinas">
+        <div className="card" id="card">
+                <img src={creatinah}/>
+                <h2>Creatina</h2>
+                <p>
+        Un suplemento deportivo súper popular, se ha demostrado que la creatina aumenta el rendimiento físico.</p>
+        </div>
+        </Link>
+
+
+ </div> 
+
+            
+      <ControlledCarousel/>
+        
+  
     <div className="div-masvendidos">
             <div className="div-masvendidos-h1">
                     <h1>Novedades - Ropa</h1>
@@ -177,7 +155,7 @@ const Home = () => {
                     <div className="card">
                         <img src={invita}/>
                         <h2>Refiere a un amigo o familiar y consigue 10$</h2>
-                        <p>Gana 10€ por cada amigo referido y ellos se llevaran envío ilimitado estandar durante los proximos 3 meses</p>
+                        <p className="empujaboton">Gana 10€ por cada amigo referido y ellos se llevaran 1 envío ilimitado</p>
                         <Link to="/ropa">
                                 <div className="button-center">
                                     <button className="button-link">Comparte</button> 
